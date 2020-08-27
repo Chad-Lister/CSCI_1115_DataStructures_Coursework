c9d446b661e85d3b1c52d249808c4f7ffa000230
Chad-Lister <59707363+Chad-Lister@users.noreply.github.com>
2020-08-27 08:17:38 -0600

    Added Chapter # 27 Folder and Java.

---
+++ Chapter # 27/Exercise 27 - 09/Exercise27_09.java
@@ -0,0 +1,34 @@
+/* 
+  Author:  Chad Lister
+  Date:  08/26/2020
+
+  (Implement hashCode for string) Write a method that returns a hashcode for string using the approach described in Section 27.3.2 with b value 31. Here is the test class with the following function header.
+
+  public static int hashCodeForString(String s)
+
+  Given: "4.5"      Expected: 51451
+  Given: "Hello"    Expected: 69609650
+*/
+
+public class Exercise27_09 { 
+  public static void main(String[] args) {
+    String s = "4.5";
+    System.out.println(hashCodeForString(s));
+    
+    s = "Hello";
+    System.out.println(hashCodeForString(s));
+  }
+  
+  public static int hashCodeForString(String s) {
+    // Add your code here
+    
+    int hashCode = 0;
+    for (int c = 0; c < s.length(); c++) {
+
+      hashCode = hashCode * 31 + s.charAt(c);
+
+    }
+    
+    return hashCode;
+  }
+}
---
+++ Chapter # 27/Exercise 27 - 11/Exercise27_11.java
@@ -0,0 +1,33 @@
+/*
+  Author:  Chad Lister
+  Date:  08/26/2020
+  
+  (setToList) Write the following method that returns an ArrayList from a set. Here is the test class. 
+
+  public static <E> ArrayList<E> setToList(Set<E> s)
+  Remember that a set will not contain duplicates.
+*/
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Exercise27_11 {
+  public static void main(String[] args) {
+    Set<String> set = new HashSet<String>(); 
+    set.add("Smith");
+    set.add("Anderson");
+    set.add("Lewis");
+    set.add("Cook"); 
+    set.add("Smith");
+    
+    ArrayList<String> list = setToList(set);
+    System.out.println(list);
+  }
+  
+  public static <E> ArrayList<E> setToList(Set<E> s) {
+    
+    ArrayList<E> list = new ArrayList<E>(s);
+    return list;
+  }
+}
